f7ae92e643588b7d519d8fdd8432843c68653547
Praveen Kumar Saini <praveenkrsaini0@gmail.com>
2019-06-05 15:05:54 +0530

    Added manifest.json for weather-pwa

--- weather-pwa/server.js
+++ weather-pwa/server.js
@@ -23,10 +23,10 @@ const fetch = require('node-fetch');
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 
 // CODELAB: Change this to add a delay (ms) before the server responds.
-const FORECAST_DELAY = 0;
+const FORECAST_DELAY = 5000;
 
 // CODELAB: If running locally, set your Dark Sky API key here
-const API_KEY = process.env.DARKSKY_API_KEY;
+const API_KEY = "dd5588773e170199097ae53131f8fcde";
 const BASE_URL = `https://api.darksky.net/forecast`;
 
 // Fake forecast data used if we can't reach the Dark Sky API
