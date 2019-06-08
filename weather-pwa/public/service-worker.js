099766fbcdb5ee74f15762ffeeae74a787c0e996
praveen-me <praveenkrsaini0@gmail.com>
2019-06-08 19:31:04 +0530

    Added fetch event to work application offline

--- weather-pwa/public/service-worker.js
+++ weather-pwa/public/service-worker.js
@@ -28,10 +28,10 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
-  evt.waitUntill(
+  evt.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('[Serice Worker] Prechaching Offline');
+        console.log('[Serice Worker] Pre-caching offline-page');
         return cache.addAll(FILES_TO_CACHE);
       })
   );
@@ -42,6 +42,17 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
   // CODELAB: Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys()
+      .then(keyList => {
+        return Promise.all(keyList.map(key => {
+          if ( key !== CACHE_NAME ) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  )
 
   self.clients.claim();
 });
@@ -49,5 +60,15 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
   // CODELAB: Add fetch event handler here.
-
+  if(evt.request.mode !== 'navigate') return;
+  evt.respondWith(
+    fetch(evt.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then(cache => {
+            console.log("it's offline");
+            return cache.match('offline.html')
+          })
+      })
+  )
 });
