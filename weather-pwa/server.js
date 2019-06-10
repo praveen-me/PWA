df4891a7ce9c99d2f80f13bcde68a31d13480249
Praveen Kumar Saini <praveenkrsaini0@gmail.com>
2019-06-10 11:48:38 +0530

    Remove the first pwa code UI from google

--- weather-pwa/server.js
+++ weather-pwa/server.js
@@ -26,7 +26,7 @@ const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 const FORECAST_DELAY = 5000;
 
 // CODELAB: If running locally, set your Dark Sky API key here
-const API_KEY = "dd5588773e170199097ae53131f8fcde";
+const API_KEY = process.env.DARKSKY_API_KEY || "dd5588773e170199097ae53131f8fcde";
 const BASE_URL = `https://api.darksky.net/forecast`;
 
 // Fake forecast data used if we can't reach the Dark Sky API
