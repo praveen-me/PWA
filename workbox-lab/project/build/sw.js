929cd9e2973b4be4850a00287e0ce11fbb144247
praveen-me <praveenkrsaini0@gmail.com>
2019-10-12 23:59:39 +0530

    Add logic for caching articles, 404 and offline static pages

--- workbox-lab/project/build/sw.js
+++ workbox-lab/project/build/sw.js
@@ -4,8 +4,78 @@ importScripts(
 
 if (workbox) {
   console.log("Yay! Workbox is loaded 🥳.");
+  workbox.precaching.precacheAndRoute([
+  {
+    "url": "style/main.css",
+    "revision": "5cc6b8b0154003ba255926700342a1b0"
+  },
+  {
+    "url": "index.html",
+    "revision": "f3b1d17a2265a150b72cf6ac6d25442f"
+  },
+  {
+    "url": "js/animation.js",
+    "revision": "732b3d64bda4cf4594650a7dbbfca326"
+  },
+  {
+    "url": "images/home/business.jpg",
+    "revision": "9c3ec8d2a8a188bab9ddc212a64a0c1e"
+  },
+  {
+    "url": "pages/offline.html",
+    "revision": "abd55f2ef953e10af7874378c96f730b"
+  },
+  {
+    "url": "pages/404.html",
+    "revision": "c21a9e328fda21f62b4c875b649ec30c"
+  }
+]);
 
-  workbox.precaching.precacheAndRoute([]);
+  workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:jpg|png|gif)/,
+    workbox.strategies.cacheFirst({
+      cacheName: "images-cache",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAge: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+
+  workbox.routing.registerRoute(
+    /\images\/icon\/*/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "icon-cache",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 5,
+          maxAge: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+
+  const articleHandler = workbox.strategies.networkFirst({
+    cacheName: "articles-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50
+      })
+    ]
+  });
+
+  workbox.routing.registerRoute(/(.*)article(.*)\.html/, args =>
+    articleHandler.handle(args).then(response => {
+      if (!response) {
+        return caches.match("pages/offline.html");
+      } else if (response.status === 404) {
+        return caches.match("pages/404.html");
+      }
+      return response;
+    })
+  );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
--- workbox-lab/project/src/sw.js
+++ workbox-lab/project/src/sw.js
@@ -4,8 +4,53 @@ importScripts(
 
 if (workbox) {
   console.log("Yay! Workbox is loaded 🥳.");
-
   workbox.precaching.precacheAndRoute([]);
+
+  workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:jpg|png|gif)/,
+    workbox.strategies.cacheFirst({
+      cacheName: "images-cache",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAge: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+
+  workbox.routing.registerRoute(
+    /\images\/icon\/*/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "icon-cache",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 5,
+          maxAge: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+
+  const articleHandler = workbox.strategies.networkFirst({
+    cacheName: "articles-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50
+      })
+    ]
+  });
+
+  workbox.routing.registerRoute(/(.*)article(.*)\.html/, args =>
+    articleHandler.handle(args).then(response => {
+      if (!response) {
+        return caches.match("pages/offline.html");
+      } else if (response.status === 404) {
+        return caches.match("pages/404.html");
+      }
+      return response;
+    })
+  );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
